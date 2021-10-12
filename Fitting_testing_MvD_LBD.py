6b136ab5d059a96bfe08cc7a38f39ce1b0bc2ce7
lbodens <lucas.bodenstein-dresler@tu-dortmund.de>
2021-10-12 14:14:54 +0200

    Update Fitting_testing_MvD_LBD.py
    just formating the file a bit (right spaces and line lenghts)

--- Fitting_testing_MvD_LBD.py
+++ Fitting_testing_MvD_LBD.py
@@ -11,44 +11,44 @@ namespace = sys._getframe(0).f_globals
 plt.style.use('seaborn-ticks')
 mpl.rcParams.update({'font.size': 16})
 
-
 """-----------------here all the functions from the other files are called and executed-----------------------"""
 input_param_file_type_str = "yaml"
 input_param_file_type = yaml
 inputs_file_name = input("please enter the name/path of the fit input file (normally Input_fit):\n")
-Inputs = input_param_file_type.load(open(inputs_file_name + "." + input_param_file_type_str), Loader=input_param_file_type.FullLoader)
-
-
+Inputs = input_param_file_type.load(open(inputs_file_name + "." + input_param_file_type_str),
+                                    Loader=input_param_file_type.FullLoader)
 
 # loading the data into a df d
 d, number_of_spectra = df_creator_main_fkt(Inputs)
 
-#just to plot once for an overview
-#plot_1st_spectra_for_overview(d)
+# just to plot once for an overview
+# plot_1st_spectra_for_overview(d)
 
 # building the Models (peak + shirley) and save it as df. The 0 is there, since we areusing the Input_fit.file. with the ana file, the number will be changed there
 mod_d, number_of_peaks, peak_func = peak_model_build_main_fkt(d, Inputs, 0)
 
-#Importing previous parameter file and check inputs
+# Importing previous parameter file and check inputs
 param_file_type_str = Inputs["param_file_type_str"]
 param_file_name = Inputs["param_file_name"]
-p4fit, p4fit_s_d, p4fit_p_d, param_file_type, param_file_type_str = param_updater_main_fkt(d, param_file_type_str, param_file_name, number_of_spectra, number_of_peaks)
+p4fit, p4fit_s_d, p4fit_p_d, param_file_type, param_file_type_str = param_updater_main_fkt(d, param_file_type_str,
+                                                                                           param_file_name,
+                                                                                           number_of_spectra,
+                                                                                           number_of_peaks)
 
-
-#plot selected spectra with the selected starting parameters, which then can be updated
+# plot selected spectra with the selected starting parameters, which then can be updated
 x = d[f'dat_0']["E"].to_numpy()
 y_d, resid = y_for_fit(d, x, number_of_spectra, number_of_peaks)
 pre_param_check = input("Now you can check the pre-set parameters. If you don´t want to do that, enter 'yes'/'y'?")
 if pre_param_check.lower() == "yes" or pre_param_check.lower() == "y":
-    params_via_plot_checking(x, d, y_d, mod_d, peak_func, param_file_type_str, param_file_name,number_of_spectra, number_of_peaks)
-
+    params_via_plot_checking(x, d, y_d, mod_d, peak_func, param_file_type_str, param_file_name, number_of_spectra,
+                             number_of_peaks)
 
 """--------------------------------------actual fitting fkt------------------------------------------------"""
-nfev = Inputs["fit_iterations"] #int(input("How many max iterations do you want to use? Please insert a number here:"))
-out, out_params,  y_d = fitting_function_main_fkt(d, p4fit, x, mod_d, number_of_spectra, number_of_peaks, nfev)
+nfev = Inputs[
+    "fit_iterations"]  # int(input("How many max iterations do you want to use? Please insert a number here:"))
+out, out_params, y_d = fitting_function_main_fkt(d, p4fit, x, mod_d, number_of_spectra, number_of_peaks, nfev)
 
 """-------------plotting the fitted spectra------------------------------"""
-
 
 fit_loop = False
 while fit_loop == False:
@@ -60,10 +60,13 @@ while fit_loop == False:
     if fit_qualtity_test.lower() == "no" or fit_qualtity_test.lower() == "n":
 
         print("Now a small loop sections will run, where you can check all init parameters via plots again.")
-        nfev = int(input("So please update the starting parameters. Furthermore: update the max iterations do you want to use here:"))
-        params_via_plot_checking(x, d, y_d, mod_d, peak_func, param_file_type_str, param_file_name, number_of_spectra, number_of_peaks)
-        out, out_params, y_d = fitting_function_main_fkt(d, p4fit, x, mod_d, number_of_spectra, number_of_peaks,nfev)
-    else:  continue
+        nfev = int(input(
+            "So please update the starting parameters. Furthermore: update the max iterations do you want to use here:"))
+        params_via_plot_checking(x, d, y_d, mod_d, peak_func, param_file_type_str, param_file_name, number_of_spectra,
+                                 number_of_peaks)
+        out, out_params, y_d = fitting_function_main_fkt(d, p4fit, x, mod_d, number_of_spectra, number_of_peaks, nfev)
+    else:
+        continue
 print(out_params)
 """-------------------------------------------------Exporting Data-----------------------------------------------------------"""
 
@@ -81,15 +84,12 @@ for p_name, p_value in out.params.items():
 param_file_type.dump(data_param_file, open("updated_test_param."+param_file_type_str, "w"))
 """
 result_file_path = Inputs["fit_result_file_path"]
-params_to_file={}
+params_to_file = {}
 for idx in range(int(number_of_peaks)):
     for i in range(int(number_of_spectra)):
         for name in out.params:
-            params_to_file[f"{name}"]= str(out.params[name])
+            params_to_file[f"{name}"] = str(out.params[name])
 param_file_type.dump(params_to_file, open(result_file_path + "." + param_file_type_str, "w"))
-
-
-
 
 """-------------------------------------------------------Gets the working directory, and the number & type of files in it------------------------------------------------------------"""
 """osdirr=[]
@@ -216,9 +216,6 @@ def sameenergyrange(filesread,eVrange):
 filesread_sameeV,eVrange = sameenergyrange(filesread,eVrange)    """
 """-----------------------------------------------------------------------------------------------------------------------------------------------------------"""
 
-
-
-
 """def PeakModel (xraw,yraw,Straightline,I1,I2):
 
     initialtime=time.time()
@@ -248,14 +245,14 @@ for a straight line background----------------"""
         Peaks = comps
         Values = out.values
         #print('values' + str(Values))
-        """#--------------Add straight line back in ----------------
+        """  # --------------Add straight line back in ----------------
 """
         Fit_BGND=Fit+BGND
-        """#--------------calculate shirley background for fitted data----------------
+        """  # --------------calculate shirley background for fitted data----------------
 """
         BGND=shirley_baseline(xraw,Fit_BGND,I1,I2)
         #print('Background '+str(BGND))
-        """#--------------check fit to raw data----------------
+        """  # --------------check fit to raw data----------------
 """
         Overall=Fit+BGND
         Diff=yraw-Overall
@@ -279,10 +276,9 @@ for a straight line background----------------"""
     report=out.fit_report
     #print(out.fit_report(min_correl=0.5))
     return (Fit, BGND, Peaks, Values, Residuals) 
-"""#------------------------------------------------------------------------------------------------------------------
+"""  # ------------------------------------------------------------------------------------------------------------------
 
-
-#-------------------------------updating the parameters in the fits--------------------------------------
+# -------------------------------updating the parameters in the fits--------------------------------------
 """
 def peak_values_update():
     read_lines = pd.read_csv("d:\\Profile\\ogd\\Desktop\\PhD\\Python\\fit_result_1.txt", header=None, skiprows=0,
@@ -308,7 +304,7 @@ def peak_values_update():
                     if math.isnan(para_test) == True:
                         print(para_name)
                         pars[para_name].set(value=para_value)
-"""#------------------------------------------------------------------------------------------------------------------
+"""  # ------------------------------------------------------------------------------------------------------------------
 """
 
 
@@ -390,7 +386,7 @@ for j in range(8):
 
 
 
-"""#-----------------------------------------------------------Plotting---------------------------------------------------------------------
+"""  # -----------------------------------------------------------Plotting---------------------------------------------------------------------
 """
 fig, ((ax1, ax2, ax3,ax4,), (ax5, ax6,ax7,ax8)) = plt.subplots(2,4)  #figsize = (height, width)
 fig.suptitle('Fitting steps',fontsize = 14, fontname='Arial')
@@ -419,4 +415,4 @@ axes[3].legend(fontsize =5,  bbox_to_anchor=(1.05, 1), loc='upper left',frameon=
 
 
 plt.show()
-"""#----------------------------------------------------------------------------------------------------------------------------------------
+"""  # ----------------------------------------------------------------------------------------------------------------------------------------
